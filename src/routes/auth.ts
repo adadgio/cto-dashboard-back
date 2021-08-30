41630e387c7906599144e6f5c6d08ac5bd451154
L. V <5218455-l.v.simplon@users.noreply.gitlab.com>
2021-08-30 16:40:28 +0200

    fix: authRouter routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,9 +5,7 @@ const TodoHandler: RequestHandler = (req, res) => res.send("TODO");
 const routeFactory = (authService: any) => {
   const router = Router();
 
-  // router.get('/projectList', TodoHandler); 
-  // router.get('/sprintList', TodoHandler);
-  // router.get('/issueList', TodoHandler);
+  router.post('/login', TodoHandler);
 
   return router;
 }
