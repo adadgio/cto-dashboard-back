18c90b8b0dc584f770285002f4ee7db9a6f2fd6d
Skeller <keller.samuel1@gmail.com>
2021-08-31 15:56:19 +0200

    Ajout auth jwtToken

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,14 +1,31 @@
-import { Router, RequestHandler } from 'express';
+import { Router } from "express";
+import ApiError from "../ApiError";
+import jwt from "jsonwebtoken";
 
-const TodoHandler: RequestHandler = (req, res) => res.send("TODO");
-
-const routeFactory = (authService: any) => {
+const routeFactory = () => {
   const router = Router();
 
-  router.post('/login', TodoHandler);
-
+  router.post("/login", (req, res, next) => {
+    try {
+      const ADMIN_NAME = process.env.ADMIN_NAME;
+      const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+      if (
+        req.body.username === ADMIN_NAME &&
+        req.body.password === ADMIN_PASSWORD
+      ) {
+        const token = jwt.sign(
+          { username: req.body.username },
+          <string>process.env.TOKEN_KEY,
+          {
+            expiresIn: "2h",
+          }
+        );
+        return res.send(token);
+      } else throw new Error("Unauthorized");
+    } catch (error) {
+      next(new ApiError(error));
+    }
+  });
   return router;
-}
-
+};
 export default routeFactory;
-
