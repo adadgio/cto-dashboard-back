7b499f7273e3e8aa57e62a9e03db720257843030
L. V <5218455-l.v.simplon@users.noreply.gitlab.com>
2021-09-01 12:00:26 +0200

    refactor: move all neo4j logic to a singleton repository

---
+++ src/DashboardRepositorySingleton.ts
@@ -0,0 +1,69 @@
+import {Issue} from '@cto-dashboard-model/cto-dashboard-model';
+import neo4j, { Driver, Session } from 'neo4j-driver';
+
+import conf from './ConfigurationSingleton';
+
+class DashboardRepository {
+  driver: Driver;
+  session: Session;
+
+  constructor() {
+    console.info("connecting to database");
+    const driver = this.driver = neo4j.driver(
+      conf.neo4jHost,
+      neo4j.auth.basic(conf.neo4jUsername, conf.neo4jPassword)
+    )
+
+    const session = this.session = driver.session({
+      database: conf.neo4jDatabase,
+      defaultAccessMode: neo4j.session.WRITE
+    })
+
+    this.setSchema();
+  }
+
+  private async setSchema() {
+    console.info("setting schema");
+
+    await this.session.run('CREATE CONSTRAINT unique_issue_id IF NOT EXISTS ON (n:Issue) ASSERT n.id IS UNIQUE')
+    await this.session.run('CREATE CONSTRAINT unique_board_id IF NOT EXISTS ON (n:Board) ASSERT n.id IS UNIQUE')
+  }
+
+  close() {
+    return this.driver.close();
+  }
+
+  async addIssuesAndBoards (issues: Issue[])  {
+  const transaction = this.session.beginTransaction();
+
+    const result = await Promise.all(
+      issues.map(issue => {
+        const queries = [ 'MERGE (:Issue {id: $id, name: $name})' ];
+        if (issue.boardId) {
+          queries.push('MERGE (:Board {id: $boardId})');
+          queries.push(`
+           MATCH (i:Issue {id: $id})
+           MATCH (b:Board {id: $boardId})
+           MERGE (i)-[:BELONGS_TO]->(b)
+         `)
+        }
+
+        console.info("merging issue", issue.id, "to board id", issue.boardId, "with query", queries);
+        return Promise.all(
+          queries.map(query =>
+            transaction.run(query, {
+            ...issue,
+            boardId: issue.boardId?.toString() || null //neo4j automatically converts ints to float
+          })
+         )
+      )
+      })
+    );
+
+    await transaction.commit();
+  }
+}
+
+const dashboardRepositorySingleton = new DashboardRepository();
+export default dashboardRepositorySingleton;
+
--- src/queryJiraAndUpdateDb.ts
+++ src/queryJiraAndUpdateDb.ts
@@ -1,24 +1,11 @@
-import neo4j from 'neo4j-driver';
-
 import conf from './ConfigurationSingleton';
 import JiraClient from './business/JiraClient';
 import * as translators from './business/JiraDataTranslators';
 import { waitAndFlatten } from './utils';
+import dashboardRepositorySingleton from './DashboardRepositorySingleton';
 
 const main = async () => {
   const jiraClient = new JiraClient(conf.jiraHost, conf.jiraUser, conf.jiraToken);
-
-  console.log("connecting to database");
-  const driver = neo4j.driver(
-    conf.neo4jHost,
-    neo4j.auth.basic(conf.neo4jUsername, conf.neo4jPassword)
-  )
-
-  console.log("acquiring session");
-  const session = driver.session({
-    database: conf.neo4jDatabase,
-    defaultAccessMode: neo4j.session.WRITE
-  })
 
 
   console.log("querying JIRA API for boards");
@@ -36,45 +23,8 @@ const main = async () => {
   console.log("all issues over", jiraBoards.length, "boards:");
   console.log(issues.map(i => i.id));
 
-
-  console.log("setting schema");
-  await session.run('CREATE CONSTRAINT unique_issue_id IF NOT EXISTS ON (n:Issue) ASSERT n.id IS UNIQUE')
-  await session.run('CREATE CONSTRAINT unique_board_id IF NOT EXISTS ON (n:Board) ASSERT n.id IS UNIQUE')
-
-  const transaction = session.beginTransaction();
-
-
-  const result = await Promise.all(
-    issues.map(issue => {
-      const queries = [ 'MERGE (:Issue {id: $id, name: $name})' ];
-      if (issue.boardId) {
-        queries.push('MERGE (:Board {id: $boardId})');
-        queries.push(`
-         MATCH (i:Issue {id: $id})
-         MATCH (b:Board {id: $boardId})
-         MERGE (i)-[:BELONGS_TO]->(b)
-       `)
-      }
-
-      console.log("merging issue", issue.id, "to board id", issue.boardId, "with query", queries);
-      return Promise.all(
-        queries.map(query =>
-          transaction.run(query, {
-          ...issue,
-          boardId: issue.boardId?.toString() || null //neo4j automatically converts ints to float
-        })
-       )
-    )
-    })
-  );
-
-  await transaction.commit();
-
-  console.log("done");
-
-
-
-  await driver.close();
+  await dashboardRepositorySingleton.addIssuesAndBoards(issues)
+  await dashboardRepositorySingleton.close();
 }
 
 main();
